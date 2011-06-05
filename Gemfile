14482508a39436c4003a14f52ef17983407c056f
Mike Zazaian <mk@mkzzn.org>
2011-06-05 12:33:42 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+group :test do
+  gem 'cucumber'
+  gem 'cucumber-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muralizer::Application.config.secret_token = '0e3de75f5075bfb6150503d771aa07dbbcb600c98925847d9abd8dadaa24ad2dd78a1b2c8e98657ee4004d7b42c34b9d3e3dfc66582696cafeb5203c48f3a36b'
